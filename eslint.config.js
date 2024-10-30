f9512421d145635e9b8aada375dc4b63f30062b1
durumi99 <kms990615@naver.com>
2024-10-30 22:50:33 +0900

    [add] Add Code Convention Packge

--- eslint.config.js
+++ eslint.config.js
@@ -4,24 +4,43 @@ import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 
 export default [
-	{ files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-	{ languageOptions: { globals: globals.browser } },
-	{
-		'jsx-runtime': {
-			plugins: ['react'],
-			parserOptions: {
-				ecmaFeatures: {
-					jsx: true,
-				},
-				jsxPragma: null, // for @typescript/eslint-parser
-			},
-			rules: {
-				'react/react-in-jsx-scope': 0,
-				'react/jsx-uses-react': 0,
-			},
-		},
-	},
-	pluginJs.configs.recommended,
-	...tseslint.configs.recommended,
-	pluginReact.configs.flat.recommended,
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    'jsx-runtime': {
+      env: {
+        browser: true,
+        es6: true,
+        node: true,
+      },
+      plugins: ['@typescript-eslint', 'react', 'prettier'],
+      extends: [
+        'prettier',
+        'airbnb',
+        'airbnb/hooks',
+        'airbnb-typescript',
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      rules: {
+        'prettier/prettier': 'error',
+        'react/react-in-jsx-scope': 0,
+        'react/prefer-stateless-function': 0,
+        'react/jsx-filename-extension': 0,
+        'react/jsx-one-expression-per-line': 0,
+        'no-nested-ternary': 0,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ];
