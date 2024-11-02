85080731d921c1914e66822deb2ab03a567b3fba
durumi99 <kms990615@naver.com>
2024-11-02 23:06:07 +0900

    #15 [feat] Home 화면 카드 종목명 추가

--- src/layout/Card/Card.tsx
+++ src/layout/Card/Card.tsx
@@ -1,10 +1,11 @@
 import { scoreToImage } from '../../utils/ScoreToImage';
 
-const Card = ({ score }: { score: number }) => {
+const Card = ({ score, stockName }: { score: number; stockName: string }) => {
   const imgLink = scoreToImage(score);
 
   return (
     <div style={{ padding: '10px' }}>
+      <div> {stockName}</div>
       <img src={imgLink}></img>
     </div>
   );
