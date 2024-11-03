d632d4bacd81960f85e6c6da6f4b377b5e4d8bb9
durumi99 <kms990615@naver.com>
2024-11-03 14:03:31 +0900

    #15 [feat] 홈화면 가로스크롤 구현 완료

--- src/layout/Mainlayout/Mainlayout.Style.ts
+++ src/layout/Mainlayout/Mainlayout.Style.ts
@@ -1,7 +1,10 @@
 import styled from '@emotion/styled';
 
+const layoutWidth = Math.min(600, window.innerWidth);
+// 추후에 반응형 수정
 const StyledMainlayout = styled('div')`
   text-align: center;
+  width: ${layoutWidth}px;
 `;
 
 export { StyledMainlayout };
