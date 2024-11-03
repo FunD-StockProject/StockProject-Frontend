c9f7e51fb1cc467e4845d4da519e606d6a5fcc76
Jadest <Jadest13x03@gmail.com>
2024-11-04 06:54:40 +0900

    #34 [add] 검색창 이벤트 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,5 @@
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { webPath } from '../router';
 import SearchBar from './SearchBar';
-import { isExistItemLocalStorage, getItemLocalStorage, setItemLocalStorage } from '../utils/LocalStorage';
 import styled from '@emotion/styled';
 
 const HeaderLogo = styled(({ src, onClick, className }: { src: string; onClick?: (e: any) => void; className?: string }) => {
@@ -13,6 +10,7 @@ const HeaderLogo = styled(({ src, onClick, className }: { src: string; onClick?:
   );
 })({
   padding: '28px 48px',
+  textAlign: 'center',
   ['img']: {
     height: '32px',
     cursor: 'pointer',
@@ -22,72 +20,20 @@ const HeaderLogo = styled(({ src, onClick, className }: { src: string; onClick?:
 const HeaderContainer = styled(({ children, className }: { children: React.ReactNode; className?: string }) => {
   return <div className={className}>{children}</div>;
 })({
-  width: '100%',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
+  padding: '0 30px',
 });
 
 const Header = () => {
   const navigate = useNavigate();
 
-  const [stockName, setStockName] = useState<string>('');
-  const [searchedData, setSearchedData] = useState<string[]>(isExistItemLocalStorage('searchedList') ? getItemLocalStorage('searchedList') : ['123', '123']);
-
-  useEffect(() => {
-    // searchedData 값이 변경될 때 localStorage에 저장
-    setItemLocalStorage('searchedList', searchedData);
-  }, [searchedData]);
-
-  const handleStockNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setStockName(e.target.value);
-  };
-
-  const handleStockNameKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      handleSearch(stockName);
-    }
-  };
-
-  const handleSearch = (stockName: string) => {
-    if (!stockName) {
-      return;
-    }
-
-    addRecentSearch(stockName);
-    navigate(webPath.search(), { state: { stockName } });
-    setStockName('');
-  };
-
-  const addRecentSearch = (stockName: string) => {
-    // 최근 검색 기록 추가
-    const nextData = Array.from(new Set([stockName, ...searchedData]));
-    if (JSON.stringify(nextData) !== JSON.stringify(searchedData)) {
-      setSearchedData(nextData);
-    }
-  };
-
-  const deleteRecentSearch = (stockName: string) => {
-    // 최근 검색 기록 삭제
-    setSearchedData((prevData) => prevData.filter((item) => item !== stockName));
-  };
-
   return (
     <>
       <HeaderContainer>
         <HeaderLogo src="/src/assets/logo.svg" onClick={() => navigate('/')} />
-        <div>
-          <SearchBar stockName={stockName} onChange={handleStockNameChange} onKeyDown={handleStockNameKeyDown}></SearchBar>
-          <button onClick={() => handleSearch(stockName)}>검색</button>
-          {searchedData.map((curStockName: string, index: number) => (
-            <span key={index}>
-              <span onClick={() => handleSearch(curStockName)}>{curStockName}</span>
-              <button onClick={() => deleteRecentSearch(curStockName)} aria-label="delete">
-                {'X'}
-              </button>
-            </span>
-          ))}
-        </div>
+        <SearchBar />
       </HeaderContainer>
     </>
   );
