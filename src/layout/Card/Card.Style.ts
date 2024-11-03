c95dbc138e014bdc130a7779b99bc9db20a5f43e
durumi99 <kms990615@naver.com>
2024-11-03 12:12:25 +0900

    #15 [feat] 홈화면 카드 리스트 구현 중

--- src/layout/Card/Card.Style.ts
+++ src/layout/Card/Card.Style.ts
@@ -1,7 +1,12 @@
 import styled from '@emotion/styled';
 
-const Button = styled.button`
-  color: turquoise;
+const StyledCard = styled('div')`
+  background-color: white;
+  margin: 0px 4.5px 10px 4.5px;
+  border: 1px solid black;
+  border-radius: 12%;
+  overflow: hidden;
+  hegith: 100px;
 `;
 
-export { Button };
+export { StyledCard };
