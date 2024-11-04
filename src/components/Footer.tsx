f80587a49a8ada894b220e366ca64627abcf8d5d
Jadest <Jadest13x03@gmail.com>
2024-11-04 21:59:55 +0900

    #34 [add] footer 기초 구현

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,22 @@
+import styled from '@emotion/styled';
+
+const FooterContainer = styled.div({
+  padding: '30px 0',
+  background: '#111111',
+});
+
+const FooterList = styled.div({
+  padding: '0 60px',
+});
 
 const Footer = () => {
   return (
     <>
-      <div>
-        푸터입니당.
-      </div>
+      <FooterContainer>
+        <FooterList>푸터입니다</FooterList>
+      </FooterContainer>
     </>
   );
-}
+};
 
 export default Footer;
