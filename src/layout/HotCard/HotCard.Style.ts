1fe85cd50bc4348bca57a1049c4252be08ec28c4
durumi99 <kms990615@naver.com>
2024-11-06 22:17:05 +0900

    #15 [feat] 반응형 구현

--- src/layout/HotCard/HotCard.Style.ts
+++ src/layout/HotCard/HotCard.Style.ts
@@ -1,34 +1,37 @@
 import styled from '@emotion/styled';
 
+// 추후 분리
+const width = Math.min(600, window.innerWidth);
+
 // 카드 컴포넌트 스타일
 const StyledCard = styled('div')`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: space-around;
   background-color: #333; /* 배경색 검정 */
   color: white; /* 텍스트 색상 흰색 */
   padding: 10px 20px;
   border-radius: 8px;
   margin: 0px 5px 10px 5px;
-  min-width: 480px;
+  width: ${width - 120}px;
 
   /* 이미지 스타일 */
   img {
     display: block;
-    height: 100px;
+    height: ${width / 6}px;
   }
 
   /* 텍스트 정렬 */
   .text {
-    font-size: 2rem;
+    font-size: 1em;
     font-weight: bold;
     text-align: center;
   }
 `;
 
 const StyledDivider = styled('div')`
-  height: 60px; /* 세로줄 길이 */
-  width: 6px; /* 세로줄 두께 */
+  height: ${width / 10}px; /* 세로줄 길이 */
+  width: ${width / 100}px; /* 세로줄 두께 */
   background-color: white; /* 세로줄 색상 */
   margin: 0 10px; /* 좌우 여백 */
 `;
