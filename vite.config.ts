ffb5ea2291fc9d67caa3ce083ca432655d6706e3
durumi99 <kms990615@naver.com>
2024-11-22 23:10:20 +0900

    [feat] deploy세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from '@svgr/rollup';
+import path from 'path';
+import { VitePluginRadar } from 'vite-plugin-radar';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePluginRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: 'G-EZPQMV95QJ',
+        disable: true,
+      },
+    }),
+  ],
   base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
