f73c8279c171bb2e4f582d522cb9f76d98f5bce8
Jadest <Jadest13x03@gmail.com>
2024-11-23 15:38:50 +0900

    #101 [fix] VitePluginRadar 지워봄

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from '@svgr/rollup';
-import { VitePluginRadar } from 'vite-plugin-radar';
+// import { VitePluginRadar } from 'vite-plugin-radar';
 // import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
     svgr(),
-    VitePluginRadar({
-      analytics: {
-        id: 'G-EZPQMV95QJ',
-      },
-    }),
+    // VitePluginRadar({
+    //   analytics: {
+    //     id: 'G-EZPQMV95QJ',
+    //   },
+    // }),
     // VitePWA({
     //   registerType: 'autoUpdate',
     //   workbox: {
