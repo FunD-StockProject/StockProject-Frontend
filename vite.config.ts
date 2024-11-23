8220c526f979c4df7c3bf6d830ac2f41d86b4d36
Jadest <Jadest13x03@gmail.com>
2024-11-23 16:23:25 +0900

    #101 [fix] config 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,57 +1,86 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from '@svgr/rollup';
-// import { VitePluginRadar } from 'vite-plugin-radar';
-// import { VitePWA } from 'vite-plugin-pwa';
+import { VitePluginRadar } from 'vite-plugin-radar';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
-    svgr(),
-    // VitePluginRadar({
-    //   analytics: {
-    //     id: 'G-EZPQMV95QJ',
-    //   },
-    // }),
     react(),
-    // VitePWA({
-    //   registerType: 'autoUpdate',
-    //   workbox: {
-    //     clientsClaim: true,
-    //     skipWaiting: true,
-    //   },
-    //   devOptions: {
-    //     enabled: true,
-    //   },
-    //   includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-    //   manifest: {
-    //     name: 'humanzipyo',
-    //     short_name: 'humanzipyo',
-    //     theme_color: '#ffffff',
-    //     icons: [
-    //       {
-    //         src: 'pwa-64x64.png',
-    //         sizes: '64x64',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: 'pwa-192x192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: 'pwa-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //         purpose: 'any',
-    //       },
-    //       {
-    //         src: 'maskable-icon-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //         purpose: 'maskable',
-    //       },
-    //     ],
-    //   },
-    // }),
+    svgr(),
+    VitePluginRadar({
+      analytics: {
+        id: 'G-EZPQMV95QJ',
+      },
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      // workbox: {
+      //   clientsClaim: true,
+      //   skipWaiting: true,
+      // },
+      // devOptions: {
+      //   enabled: true,
+      // },
+      manifest: {
+        name: 'humanzipyo',
+        short_name: 'humanzipyo',
+        description: '프로젝트 설명',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        prefer_related_applications: true,
+        lang: 'ko',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        // icons: [
+        //   {
+        //     src: 'pwa-64x64.png',
+        //     sizes: '64x64',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: 'pwa-192x192.png',
+        //     sizes: '192x192',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: 'pwa-512x512.png',
+        //     sizes: '512x512',
+        //     type: 'image/png',
+        //     purpose: 'any',
+        //   },
+        //   {
+        //     src: 'maskable-icon-512x512.png',
+        //     sizes: '512x512',
+        //     type: 'image/png',
+        //     purpose: 'maskable',
+        //   },
+        // ],
+      },
+    }),
   ],
 });
