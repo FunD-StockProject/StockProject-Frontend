923598d5464d47d0cd08aa67ab8a80acc8f4fbd2
durumi99 <kms990615@naver.com>
2024-11-23 14:04:09 +0900

    #89 [feat] plugin 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,44 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from '@svgr/rollup';
 import { VitePluginRadar } from 'vite-plugin-radar';
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+const plugin = <Partial<VitePWAOptions>>{
+  registerType: 'autoUpdate',
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+  manifest: {
+    name: 'humanzipyo',
+    short_name: 'humanzipyo',
+    start_url: '/',
+    display: 'standalone',
+    theme_color: '#5270FF',
+    background_color: '#ffffff',
+    icons: [
+      {
+        src: 'pwa-64x64.png',
+        sizes: '64x64',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: 'maskable-icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+    ],
+  },
+};
 
 export default defineConfig({
   plugins: [
@@ -13,41 +50,6 @@ export default defineConfig({
         id: 'G-EZPQMV95QJ',
       },
     }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-      manifest: {
-        name: 'humanzipyo',
-        short_name: 'humanzipyo',
-        start_url: '/',
-        display: 'standalone',
-        theme_color: '#5270FF',
-        background_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-64x64.png',
-            sizes: '64x64',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any',
-          },
-          {
-            src: 'maskable-icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-        ],
-      },
-    }),
+    VitePWA(plugin),
   ],
 });
