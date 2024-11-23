413c917ef09bfc7efc737bc9f2122aabcfe71650
durumi99 <kms990615@naver.com>
2024-11-23 15:38:23 +0900

    #89 [feat] config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
-    svgr(),
+    svgr(), // SVG 파일을 React 컴포넌트로 변환하는 플러그인
     react(),
     VitePluginRadar({
       analytics: {
@@ -22,6 +22,7 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      // SVG 파일을 PWA 자산에 포함하지 않도록 수정
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'humanzipyo',
