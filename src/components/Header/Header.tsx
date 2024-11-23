509f8c5fb342e6ed1795f53a001b38f2f87e7beb
Jadest <Jadest13x03@gmail.com>
2024-11-23 19:32:22 +0900

    #89 [fix] svg 파일 import 형식 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 
 import SearchBar from '../SearchBar/SearchBar';
 
-import { ReactComponent as LogoSVG } from '../../assets/logo_white.svg';
+import LogoSVG from '../../assets/logo_white.svg?react';
 import { HeaderContainer, HeaderContents, HeaderLogo } from './Header.Style';
 
 const Header = () => {
