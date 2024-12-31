373b99e06b09e88488c8388c298671134de9150e
durumi99 <kms990615@naver.com>
2024-12-31 17:51:59 +0900

    #37 [feat] 폰트 전역 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
+import './App.css';
 import { router } from './router';
 
+// App.css 파일을 import
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
