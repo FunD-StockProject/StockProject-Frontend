a146633208d6224627872fc533fad5d4cdba894d
Jadest <Jadest13x03@gmail.com>
2025-01-06 10:54:21 +0900

    #66 [fix] macOS, iOS Safari, instagram inapp browser 워드클라우드 깨짐 현상 해결

--- src/components/StockWordCloud/StockWordCloud.Worker.ts
+++ src/components/StockWordCloud/StockWordCloud.Worker.ts
@@ -1,21 +1,14 @@
-import { WordFrequency } from './StockWordCloud.Type';
+// import { WordFrequency } from './StockWordCloud.Type';
 
 export const testWorker = new Worker(new URL('../../utils/worker/GenerateWordCloud.ts', import.meta.url), {
   type: 'module',
 });
 
-export const generateWordCloud = ({
-  data,
-  width,
-  height,
-}: {
-  data: WordFrequency[];
-  width: number;
-  height: number;
-}) => {
+export const generateWordCloud = ({ data, width, height, adjust }: any) => {
   testWorker.postMessage({
     data: data,
     width: width,
     height: height,
+    adjust: adjust,
   });
 };
