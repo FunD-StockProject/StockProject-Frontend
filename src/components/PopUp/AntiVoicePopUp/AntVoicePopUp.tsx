ad7b61b24af8fc22dc55eb37930dc2b5ec8c23e9
Jadest13 <jadest13x03@gmail.com>
2025-12-01 22:04:06 +0900

    #404 [fix] 자주 언급되는 단어 팝업 수정

--- src/components/PopUp/AntiVoicePopUp/AntVoicePopUp.tsx
+++ src/components/PopUp/AntiVoicePopUp/AntVoicePopUp.tsx
@@ -1,52 +1,25 @@
+import { theme } from '@styles/themes';
+import AntVoicePNG from '@assets/design/antVoice.png';
 import CommonPopUp from '../CommonPopUp';
-import { PopUpContent, PopUpTitle } from '../CommonPopUp.style';
-import {
-  PopUpDetail,
-  PopUpDetailContainer,
-  PopUpDetailNumber,
-  PopUpDetailWord,
-  PopUpImage,
-} from './AntVoicePopUp.style';
+import { PopUpTitle } from '../CommonPopUp.style';
 
 const AntiVoicePopUp = ({ onClose }: { onClose: () => void }) => (
   <CommonPopUp onClose={onClose}>
-    <PopUpTitle>개미들의 목소리란?</PopUpTitle>
-    <PopUpContent>
-      각종 커뮤니티의 댓글을 한눈에 볼 수 있는 워드클라우드에요.
-      <PopUpImage>
-        <div>
-          <PopUpDetailNumber color={'grayscale80'}>1</PopUpDetailNumber>
-          <PopUpDetailWord>
-            가장 많이
-            <br /> 언급된 단어
-          </PopUpDetailWord>
-        </div>
-
-        <div>
-          <PopUpDetailNumber color={'grayscale80'}>2</PopUpDetailNumber>
-          <PopUpDetailWord color={'grayscale60'} fontSize={24} textAlign={'right'}>
-            상대적으로 적게
-            <br /> 언급된 단어
-          </PopUpDetailWord>
-        </div>
-      </PopUpImage>
-      <PopUpDetailContainer>
-        <PopUpDetail>
-          <PopUpDetailNumber>1</PopUpDetailNumber>
-          <span>
-            크기가 클수록 각종 커뮤니티에서
-            <br /> 가장 많이 언급된 단어예요.
-          </span>
-        </PopUpDetail>
-        <PopUpDetail>
-          <PopUpDetailNumber>2</PopUpDetailNumber>
-          <span>
-            크기가 작을수록 각종 커뮤니티에서
-            <br /> 상대적으로 적게 언급된 단어에요
-          </span>
-        </PopUpDetail>
-      </PopUpDetailContainer>
-    </PopUpContent>
+    <PopUpTitle>자주 언급되는 단어란?</PopUpTitle>
+    <img src={AntVoicePNG} />
+    <div
+      style={{
+        margin: '0',
+        ...theme.font.detail12Semibold,
+        color: theme.colors.sub_gray11,
+        padding: '16px 12px',
+        background: theme.colors.sub_white,
+        borderRadius: '8px',
+      }}
+    >
+      각종 커뮤니티 댓글을 한눈에 볼 수 있는 워드클라우드입니다. 크기가 클수록 각종 커뮤니티에서 가장 많이 언급된
+      단어입니다.
+    </div>
   </CommonPopUp>
 );
 
