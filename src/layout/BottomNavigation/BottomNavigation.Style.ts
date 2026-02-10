d2d1ce8f0e8d0966263f2571fca043372eb8d9b8
durumi99 <kms990615@naver.com>
2026-02-10 20:56:25 +0900

    feat: 전역 css 수정

--- src/layout/BottomNavigation/BottomNavigation.Style.ts
+++ src/layout/BottomNavigation/BottomNavigation.Style.ts
@@ -10,10 +10,16 @@ const NavContainer = styled('nav')({
   backgroundColor: theme.colors.sub_black,
   justifyContent: 'space-between',
   alignItems: 'center',
-  padding: '20px 33px 30px',
+  padding: '16px 33px',
+  paddingBottom: 'max(16px, env(safe-area-inset-bottom))',
   boxSizing: 'border-box',
   zIndex: '10',
   boxShadow: '0px -2px 10px 0px rgba(255, 255, 255, 0.05)',
+  // 웹뷰 최적화
+  WebkitBackfaceVisibility: 'hidden',
+  backfaceVisibility: 'hidden',
+  transform: 'translateZ(0)', // GPU 가속
+  willChange: 'transform',
 });
 
 const NavItem = styled('div')<{ isActive: boolean }>(({ isActive }) => ({
